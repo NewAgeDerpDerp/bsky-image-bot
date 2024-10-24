75e49d91cfac57bb28f24c19af76eca76fdcb5fd
NewAgeDerpDerp <38193901+NewAgeDerpDerp@users.noreply.github.com>
2024-10-24 09:18:25 -0400

    Add files via upload

--- src/index.ts
+++ src/index.ts
@@ -6,18 +6,19 @@ dotenv.config();
 // EDIT THIS!
 function postTextFromImageName(imageName: string): string {
   // Remove the file extension and parse the date
-  const dateParts = imageName.replace('.jpg', '').split('-');
-  const date = new Date(Number(dateParts[0]), Number(dateParts[1]) - 1, Number(dateParts[2] || 1));
+//   const dateParts = imageName.replace('.jpg', '').split('-');
+//   const date = new Date(Number(dateParts[0]), Number(dateParts[1]) - 1, Number(dateParts[2] || 1));
 
-  // Create a formatter
-  const formatter = new Intl.DateTimeFormat('en-US', {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-  });
+//   // Create a formatter
+//   const formatter = new Intl.DateTimeFormat('en-US', {
+//     year: 'numeric',
+//     month: 'long',
+//     day: 'numeric',
+//   });
 
-  // Format the date
-  return formatter.format(date);
+//   // Format the date
+//   return formatter.format(date);
+  return 'i dont need this lmao';
 }
 
 // EDIT THIS!
